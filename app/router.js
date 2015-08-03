ac0e3f0bc00c9b91f9bc1bf813dca6fa084036cc
Mitchell Valine <mitchellvaline@yahoo.com>
2015-08-03 15:39:13 -0700

    simple auth implemented

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('login');
   this.route('posts', function() {
     this.route('new');
     this.route('post', { path: '/:post_id' }, function() {
