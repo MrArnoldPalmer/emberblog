c701cd1de13b6fca7a33c7d67d210005d84dfca3
Mitchell Valine <mitchellvaline@yahoo.com>
2015-08-03 21:00:51 -0700

    semantic included, some styling

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,6 +18,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
+  app.import('bower_components/semantic-ui/dist/semantic.js');
+  app.import('bower_components/semantic-ui/dist/semantic.css');
   return app.toTree();
 };
